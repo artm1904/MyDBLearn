838ffa2d3029cd161382787fae0b002e6270d625
artm1904 <151929006+artm1904@users.noreply.github.com>
2023-12-13 16:11:27 +0300

    Update MyDbApplication.java

--- src/main/java/ru/mpei/MyDB/MyDbApplication.java
+++ src/main/java/ru/mpei/MyDB/MyDbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyDbApplication {
 
 	public static void main(String[] args) {
+		System.out.print("Hello!");
 		SpringApplication.run(MyDbApplication.class, args);
 	}
 
